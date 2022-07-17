1aabeb638c70590f27cfc5279ead265ac06a2230
almeida <almeida@almeida.lima>
2022-07-17 19:28:14 -0300

    How use slincing in python

---
+++ pytutorial_slincing.py
@@ -0,0 +1,12 @@
+#Slicing Strings - Fatiando Strings
+
+p = "Hello-Word!"
+
+#slincing of second position to fifith position - fatiando da segunda posição até a quinta posição
+print(p[2:5])  
+#slincing of fifith position for the beginnning - fatiando da quinta posição até o inicio
+print(p[:5])
+#slincing of second position for the end - fatiando da segunda posição até o inicio
+print(p[2:])
+#slincing of fifith position for the beginnning and second position for the end - fatiando da quinta posição para o começo e da segunda posição para o final
+print(p[-5:-2])
